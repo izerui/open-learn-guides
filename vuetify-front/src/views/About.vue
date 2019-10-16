49503bc93b0f3140118d65334c6187ab87eac536
serv <yifeng2000@126.com>
2019-10-16 23:14:38 +0800

    33

--- vuetify-front/src/views/About.vue
+++ vuetify-front/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>you know.</h1>
+    <h2>好用请支持一下!</h2>
+    <img src="../images/WechatIMG327.jpeg" width="300" height="400"></img>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="../images/WechatIMG326.jpeg" width="300" height="400"></img>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="../images/WechatIMG325.jpeg" width="300" height="400"></img>
   </div>
 </template>
